f5bf3006e01c41b3b2312a29ddb18fdcd2910093
Shubhankar <shubhosen98@gmail.com>
2021-07-25 13:04:55 +0530

    added a function to the JS file for sum

--- app.js
+++ app.js
@@ -16,3 +16,24 @@ let set = Name(["Shubho", "Ashit", "Gate"]);
 
 console.log(set);
 
+
+
+
+function sum(numArr) {
+
+    let total = 0;
+
+    for (let i=0; i <numArr.length; i++) 
+    
+    {
+        total += numArr[i];
+     }
+    
+    return total;
+}
+
+console.log(sum([1,2,3,4,5,6,7,8]));
+
+
+
+
